f79c6673ed92f955dc8f0d2272e60e5525e0b220
Edmund White <edmund.white_at_hotmaildotcom>
2012-10-08 18:35:42 +0100

    Initial state of file

---
+++ prime.groovy
@@ -0,0 +1,16 @@
+String str = System.console().readline();
+int num =Integer.parseInt(str);
+int divr=1;
+while(divr<num){
+    divr++;
+    if(num%divr -=0){
+        println "The number is not prime";
+        break;
+    }
+    else
+    {
+        println "The number is prime";
+        break;
+      }
+    }
+  
